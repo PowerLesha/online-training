56d0c99bc60966ed8dba567134bc684dab92573a
Lesha <leshapowerlifter@gmail.com>
2024-01-25 17:11:26 +0200

    added full logic for submitting form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useRef } from "react";
 import emailjs from "@emailjs/browser";
 import style from "./ContactForm.module.css";
-import { Button } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
+import Pre from "../Pre";
 
 const ContactForm = () => {
   const form = useRef();
   const [step, setStep] = useState(1);
-
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
   // State for form fields
   const [formData, setFormData] = useState({
     first_name: "",
@@ -20,25 +22,71 @@ const ContactForm = () => {
     best_dl: "",
     message: "",
   });
+  const [formErrors, setFormErrors] = useState({
+    first_name: false,
+    last_name: false,
+    from_email: false,
+    phone: false,
+    instagram_telegram: false,
+    body_weight: false,
+    best_sq: false,
+    best_bp: false,
+    best_dl: false,
+  });
 
-  const sendEmail = (e) => {
+  const [errorMessages] = useState({
+    first_name: "First name is required",
+    last_name: "Last name is required",
+    from_email: "Email is required",
+    phone: "Phone is required",
+    body_weight: "Body weight is required",
+    best_sq: "Best squat is required",
+    best_bp: "Best bench is required",
+    best_dl: "Best deadlift is required",
+  });
+
+  const sendEmail = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    const errors = {
+      first_name: formData.first_name.trim() === "",
+      last_name: formData.last_name.trim() === "",
+      from_email: formData.from_email.trim() === "",
+      phone: formData.phone.trim() === "",
+      body_weight: formData.body_weight.trim() === "",
+      best_sq: formData.best_sq.trim() === "",
+      best_bp: formData.best_bp.trim() === "",
+      best_dl: formData.best_dl.trim() === "",
+    };
 
-    emailjs
-      .send(
+    setFormErrors(errors);
+    if (Object.values(errors).includes(true)) {
+      setLoading(false);
+      if (formErrors.first_name || formErrors.last_name) {
+        setStep(1);
+        return;
+      }
+      if (formErrors.phone || formErrors.from_email) {
+        setStep(2);
+        return;
+      } else setStep(3);
+      return;
+    }
+    try {
+      await emailjs.send(
         "service_kj8pd1c",
         "template_dyqopou",
         formData,
         "BPwHVQ2oWCbe07BQG"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
       );
+
+      // Show the modal on successful submission
+      setShowModal(true);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
     setFormData({
       first_name: "",
       last_name: "",
@@ -62,183 +110,305 @@ const ContactForm = () => {
   };
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: value.trim() === "",
+    }));
   };
-
+  const handleCloseModal = () => {
+    // Close the modal and reset any necessary state
+    setShowModal(false);
+  };
   return (
-    <form ref={form} onSubmit={sendEmail} className={style.msform}>
-      {/* progressbar */}
-      <ul className={style.progressBar}>
-        <li
-          className={step === 1 || step === 2 || step === 3 ? style.active : ""}
-          onClick={() => setStep(1)}
-        >
-          Account Setup
-        </li>
-        <li
-          className={step === 2 || step === 3 ? style.active : ""}
-          onClick={() => setStep(2)}
-        >
-          Social Profiles
-        </li>
-        <li
-          className={step === 3 ? style.active : ""}
-          onClick={() => setStep(3)}
-        >
-          Personal Details
-        </li>
-      </ul>
-
-      {/* fieldsets */}
-      {step === 1 && (
-        <fieldset className={style.fieldset}>
-          <h2 className={style.fsTitle}>Please fill out this information</h2>
-          <h3 className={style.fsSubtitle}>This is step 1</h3>
-          <label className={style.fsTitle}> First Name</label>
-          <input
-            type="text"
-            name="first_name"
-            className={style.input}
-            value={formData.first_name}
-            onChange={handleChange}
-          />
-          <label className={style.fsTitle}> Last Name</label>
-          <input
-            type="text"
-            name="last_name"
-            className={style.input}
-            value={formData.last_name}
-            onChange={handleChange}
-          />
-
-          <Button
-            type="button"
-            name="next"
-            value="Next"
-            onClick={handleNext}
-            className={style.button_next}
+    <>
+      <form ref={form} onSubmit={sendEmail} className={style.msform}>
+        {/* progressbar */}
+        <ul className={style.progressBar}>
+          <li
+            className={
+              step === 1 || step === 2 || step === 3 ? style.active : ""
+            }
+            onClick={() => setStep(1)}
           >
-            Next
-          </Button>
-        </fieldset>
-      )}
-
-      {step === 2 && (
-        <fieldset className={style.fieldset}>
-          <h2 className={style.fsTitle}>Social Profiles</h2>
-          <h3 className={style.fsSubtitle}>
-            Your presence on the social network
-          </h3>
-          <label className={style.fsTitle}>Email</label>
-          <input
-            type="email"
-            name="from_email"
-            className={style.input}
-            value={formData.from_email}
-            onChange={handleChange}
-          />
-          <label className={style.fsTitle}>Phone</label>
-          <input
-            type="text"
-            name="phone"
-            className={style.input}
-            value={formData.phone}
-            onChange={handleChange}
-          />
-          <label className={style.fsTitle}>Instagram/Telegram</label>
-          <input
-            type="text"
-            name="instagram_telegram"
-            className={style.input}
-            value={formData.instagram_telegram}
-            onChange={handleChange}
-          />
-
-          <Button
-            type="button"
-            name="previous"
-            value="Previous"
-            onClick={handlePrevious}
-            className={style.button_next}
+            Account Setup
+          </li>
+          <li
+            className={step === 2 || step === 3 ? style.active : ""}
+            onClick={() => setStep(2)}
           >
-            {" "}
-            Previous
-          </Button>
-          <Button
-            type="button"
-            name="next"
-            value="Next"
-            onClick={handleNext}
-            className={style.button}
+            Social Profiles
+          </li>
+          <li
+            className={step === 3 ? style.active : ""}
+            onClick={() => setStep(3)}
           >
-            {" "}
-            Next{" "}
-          </Button>
-        </fieldset>
-      )}
+            Personal Details
+          </li>
+        </ul>
 
-      {step === 3 && (
-        <fieldset className={style.fieldset}>
-          <h2 className={style.fsTitle}>Personal Details</h2>
-          <h3 className={style.fsSubtitle}>
-            Please type your current body weight and results in kg
-          </h3>
+        {/* fieldsets */}
+        {step === 1 && (
+          <fieldset className={style.fieldset}>
+            <h2 className={style.fsTitle}>Please fill out this information</h2>
+            <h3 className={style.fsSubtitle}>This is step 1</h3>
+            <label className={style.fsTitle}>
+              {" "}
+              First Name <span className={style.form_name}> *</span>
+            </label>
+            <input
+              type="text"
+              name="first_name"
+              className={
+                formErrors.first_name
+                  ? `${style.input} ${style.error}`
+                  : style.input
+              }
+              value={formData.first_name}
+              onChange={handleChange}
+            />
+            {formErrors.first_name && (
+              <span className={style.error_message}>
+                {errorMessages.first_name}
+              </span>
+            )}
+            <label className={style.fsTitle}>
+              {" "}
+              Last Name <span className={style.form_name}> *</span>
+            </label>
+            <input
+              type="text"
+              name="last_name"
+              className={
+                formErrors.last_name
+                  ? `${style.input} ${style.error}`
+                  : style.input
+              }
+              value={formData.last_name}
+              onChange={handleChange}
+            />
+            {formErrors.last_name && (
+              <span className={style.error_message}>
+                {errorMessages.last_name}
+              </span>
+            )}
 
-          <label className={style.fsTitle}>Body weight</label>
-          <input
-            type="text"
-            name="body_weight"
-            className={style.input}
-            value={formData.body_weight}
-            onChange={handleChange}
-          />
-          <label className={style.fsTitle}>Best squat</label>
-          <input
-            type="text"
-            name="best_sq"
-            className={style.input}
-            value={formData.best_sq}
-            onChange={handleChange}
-          />
-          <label className={style.fsTitle}>Best bench-press</label>
-          <input
-            type="text"
-            name="best_bp"
-            className={style.input}
-            value={formData.best_bp}
-            onChange={handleChange}
-          />
-          <label className={style.fsTitle}>Best deadlift</label>
-          <input
-            type="text"
-            name="best_dl"
-            className={style.input}
-            value={formData.best_dl}
-            onChange={handleChange}
-          />
-          <label className={style.fsTitle}>Message</label>
-          <textarea
-            name="message"
-            className={style.input}
-            value={formData.message}
-            onChange={handleChange}
-          ></textarea>
-          <Button
-            type="button"
-            name="previous"
-            value="Previous"
-            onClick={handlePrevious}
-            className={style.button_next}
-          >
-            {" "}
-            Previous{" "}
+            <Button
+              type="button"
+              name="next"
+              value="Next"
+              onClick={handleNext}
+              className={style.button_next}
+            >
+              Next
+            </Button>
+          </fieldset>
+        )}
+
+        {step === 2 && (
+          <fieldset className={style.fieldset}>
+            <h2 className={style.fsTitle}>Social Profiles</h2>
+            <h3 className={style.fsSubtitle}>
+              Your presence on the social network
+            </h3>
+            <label className={style.fsTitle}>
+              Email <span className={style.form_name}> *</span>
+            </label>
+            <input
+              type="email"
+              name="from_email"
+              className={
+                formErrors.from_email
+                  ? `${style.input} ${style.error}`
+                  : style.input
+              }
+              value={formData.from_email}
+              onChange={handleChange}
+            />
+            {formErrors.from_email && (
+              <span className={style.error_message}>
+                {errorMessages.from_email}
+              </span>
+            )}
+            <label className={style.fsTitle}>
+              Phone <span className={style.form_name}> *</span>
+            </label>
+            <input
+              type="text"
+              name="phone"
+              className={
+                formErrors.phone ? `${style.input} ${style.error}` : style.input
+              }
+              value={formData.phone}
+              onChange={handleChange}
+            />
+            {formErrors.phone && (
+              <span className={style.error_message}>{errorMessages.phone}</span>
+            )}
+            <label className={style.fsTitle}>Instagram/Telegram</label>
+            <input
+              type="text"
+              name="instagram_telegram"
+              className={style.input}
+              value={formData.instagram_telegram}
+              onChange={handleChange}
+            />
+
+            <Button
+              type="button"
+              name="previous"
+              value="Previous"
+              onClick={handlePrevious}
+              className={style.button_next}
+            >
+              {" "}
+              Previous
+            </Button>
+            <Button
+              type="button"
+              name="next"
+              value="Next"
+              onClick={handleNext}
+              className={style.button}
+            >
+              {" "}
+              Next{" "}
+            </Button>
+          </fieldset>
+        )}
+
+        {step === 3 && (
+          <fieldset className={style.fieldset}>
+            <h2 className={style.fsTitle}>Personal Details</h2>
+            <h3 className={style.fsSubtitle}>
+              Please type your current body weight and results in kg
+            </h3>
+
+            <label className={style.fsTitle}>
+              Body weight <span className={style.form_name}> *</span>
+            </label>
+
+            <input
+              type="text"
+              name="body_weight"
+              className={
+                formErrors.body_weight
+                  ? `${style.input} ${style.error}`
+                  : style.input
+              }
+              value={formData.body_weight}
+              onChange={handleChange}
+            />
+            {formErrors.body_weight && (
+              <span className={style.error_message}>
+                {errorMessages.body_weight}
+              </span>
+            )}
+
+            <label className={style.fsTitle}>
+              Best squat <span className={style.form_name}> *</span>
+            </label>
+            <input
+              type="text"
+              name="best_sq"
+              className={
+                formErrors.best_sq
+                  ? `${style.input} ${style.error}`
+                  : style.input
+              }
+              value={formData.best_sq}
+              onChange={handleChange}
+            />
+            {formErrors.best_sq && (
+              <span className={style.error_message}>
+                {errorMessages.best_sq}
+              </span>
+            )}
+            <label className={style.fsTitle}>
+              Best bench-press <span className={style.form_name}> *</span>
+            </label>
+            <input
+              type="text"
+              name="best_bp"
+              className={
+                formErrors.best_bp
+                  ? `${style.input} ${style.error}`
+                  : style.input
+              }
+              value={formData.best_bp}
+              onChange={handleChange}
+            />
+            {formErrors.best_bp && (
+              <span className={style.error_message}>
+                {errorMessages.best_bp}
+              </span>
+            )}
+            <label className={style.fsTitle}>
+              Best deadlift <span className={style.form_name}> *</span>
+            </label>
+            <input
+              type="text"
+              name="best_dl"
+              className={
+                formErrors.best_dl
+                  ? `${style.input} ${style.error}`
+                  : style.input
+              }
+              value={formData.best_dl}
+              onChange={handleChange}
+            />
+            {formErrors.best_dl && (
+              <span className={style.error_message}>
+                {errorMessages.best_dl}
+              </span>
+            )}
+            <label className={style.fsTitle}>Message</label>
+            <textarea
+              name="message"
+              className={style.input}
+              value={formData.message}
+              onChange={handleChange}
+            ></textarea>
+            <Button
+              type="button"
+              name="previous"
+              value="Previous"
+              onClick={handlePrevious}
+              className={style.button_next}
+            >
+              {" "}
+              Previous{" "}
+            </Button>
+            <Button type="submit" className={style.button}>
+              Submit
+            </Button>
+          </fieldset>
+        )}
+      </form>
+      <Modal className={style.modal} show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Submission Successful</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Thank you for submitting the form. Your information has been received.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleCloseModal}>
+            Close
           </Button>
-          <Button type="submit" className={style.button}>
-            Submit
-          </Button>
-        </fieldset>
-      )}
-    </form>
+        </Modal.Footer>
+      </Modal>
+
+      <div className={style.preloaderOverlay}>
+        <Pre load={loading} />
+      </div>
+    </>
   );
 };
 
